cf5855e937f05a7d6c66bf4c4eb41ce8b8164105
SteMak <chestedos@gmail.com>
2023-07-22 23:17:33 +0200

    finalize contracts

--- contracts/Ensure.sol
+++ contracts/Ensure.sol
@@ -1,10 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+/**
+ * @notice Requires `condition` to be true; otherwise, throws CUSTOM_ERROR.
+ *
+ * @dev When `revert CUSTOM_ERROR()` is called, it throws a bytes error message in the format:
+ * - 4 bytes: CUSTOM_ERROR selector
+ * - 32 bytes * parameters.length: CUSTOM_ERROR parameters
+ *
+ * @param condition Expression to be validated
+ * @param data Packed CUSTOM_ERROR selector and parameters
+ * @dev The first 28 bytes of `data` are empty because the 4 bytes selector is converted to uint, and uints are aligned
+ *      on the right in packed representation.
+ * @dev The `data` could be generated via `abi.encode(uint32(CUSTOM_ERROR selector), CUSTOM_ERROR parameters)`.
+ */
 function ensure(bool condition, bytes memory data) pure {
   if (!condition) {
     // solhint-disable-next-line no-inline-assembly
     assembly {
+      /**
+       * @dev Expected `data` layout produced by
+       *      `abi.encode(uint32(CUSTOM_ERROR selector), CUSTOM_ERROR parameters)`:
+       * - 32 bytes: length
+       * - 28 bytes: empty space
+       * - 4 bytes: CUSTOM_ERROR selector
+       * - 32 bytes * parameters.length: CUSTOM_ERROR parameters
+       */
+
+      // `add(data, 60) = add(data, add(32, 28))`: CUSTOM_ERROR selector start pointer
+      // `sub(mload(data), 28)`: length after trimming empty space
       revert(add(data, 60), sub(mload(data), 28))
     }
   }
--- contracts/Example.sol
+++ contracts/Example.sol
@@ -3,9 +3,34 @@ pragma solidity ^0.8.4;
 
 import "./Ensure.sol";
 
+/**
+ * @title Example errors contract contains error-related functionality to be used in the example target contract
+ *
+ * @notice The errors and functions may be defined outside of any contract; however, in such case, it looks better if
+ *      they are defined in a separate file and imported via `import "PATH" as Errors` pattern
+ *
+ * @notice The different errors are implemented to check that packing works as required
+ *
+ * @dev Use `build_CUSTOM_ERROR` functions to build CUSTOM_ERROR data properly
+ */
 abstract contract ExampleErrors {
+  /// @notice Typical error when a uint value is not within the specified bounds
   error NotWithinBounds(uint256 min, uint256 max, uint256 actual);
 
+  /**
+   * @notice Builds `NotWithinBounds` error data for `ensure` function call
+   *
+   * @dev The function performs necessary type checks of the error parameters.
+   *      Direct call to `abi.encode` does not perform any type checks.
+   * @dev The function parameters should be fully consistent with error parameters.
+   *
+   * @param min Error parameter representing the minimum allowed value
+   * @param max Error parameter representing the maximum allowed value
+   * @param actual Error parameter representing the actual provided value
+   *
+   * @return The bytes array is returned from the function:
+   *      28 empty bytes + bytes `NotWithinBounds` error representation
+   */
   function buildNotWithinBounds(uint256 min, uint256 max, uint256 actual) internal pure returns (bytes memory) {
     return abi.encode(uint32(NotWithinBounds.selector), min, max, actual);
   }
@@ -23,7 +48,22 @@ abstract contract ExampleErrors {
   }
 }
 
+/**
+ * @title Example target contract demonstrates `ensure` function usage
+ *
+ * @notice The different functions are implemented to check that packing works as required
+ *
+ * @dev The `if (!condition) revert CUSTOM_ERROR()` may save some Gas comparing to the implementation due to error
+ *      messages not being precalculated on the `condition` check stage
+ */
 contract ExampleTarget is ExampleErrors {
+  /**
+   * @notice Example function calls `ensure` function to validate that the `value` parameter is in provided boundaries
+   *
+   * @param min Parameter representing the bottom boundary
+   * @param max Parameter representing the top boundary
+   * @param value Parameter representing the actual provided value to be validated
+   */
   function checkBounds(uint256 min, uint256 max, uint256 value) external pure {
     ensure(min <= value && value <= max, buildNotWithinBounds(min, max, value));
   }
@@ -33,6 +73,8 @@ contract ExampleTarget is ExampleErrors {
   }
 
   function checkBytesHead(bytes4 expected, bytes32 value) external pure {
-    ensure(bytes4(value) == expected, buildHeadBytesMismatch(expected, bytes4(value)));
+    /// @dev `bytes4(bytes32 value)`: trims 28 bytes tail
+    bytes4 head = bytes4(value);
+    ensure(head == expected, buildHeadBytesMismatch(expected, head));
   }
 }
